99e10a4ecba1bfa1e036ec50c80af827d21fa853
Antoine Labard <antoine@labard.fr>
2022-05-25 10:08:06 +0200

    feat: ✨ implement getCoinById in CoinPaprikaApi

--- app/src/main/java/fr/labard/cryptocurrencyapp/data/remote/CoinPaprikaApi.kt
+++ app/src/main/java/fr/labard/cryptocurrencyapp/data/remote/CoinPaprikaApi.kt
@@ -1,9 +1,14 @@
 package fr.labard.cryptocurrencyapp.data.remote
 
+import fr.labard.cryptocurrencyapp.data.remote.dto.CoinDetailDto
 import fr.labard.cryptocurrencyapp.data.remote.dto.CoinDto
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface CoinPaprikaApi {
     @GET("/v1/coins")
     suspend fun getCoins(): List<CoinDto>
+
+    @GET("/v1/coins/{coindId}")
+    suspend fun getCoinById(@Path("coinId") coindId: String): CoinDetailDto
 }
