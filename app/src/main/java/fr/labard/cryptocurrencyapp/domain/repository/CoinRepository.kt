c6e28e6fb29d14ecd0e420484e6eb6459371e0ab
Antoine Labard <antoine@labard.fr>
2022-05-25 10:16:19 +0200

    feat: ✨ implement CoinRepository interface

---
+++ app/src/main/java/fr/labard/cryptocurrencyapp/domain/repository/CoinRepository.kt
@@ -0,0 +1,9 @@
+package fr.labard.cryptocurrencyapp.domain.repository
+
+import fr.labard.cryptocurrencyapp.data.remote.dto.CoinDetailDto
+import fr.labard.cryptocurrencyapp.data.remote.dto.CoinDto
+
+interface CoinRepository {
+    suspend fun getCoins(): List<CoinDto>
+    suspend fun getCoinById(): CoinDetailDto
+}
