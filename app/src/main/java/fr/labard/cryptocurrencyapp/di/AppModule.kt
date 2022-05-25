a8253df8b145276c55f0c7b912f3fc2957a1339c
Antoine Labard <antoine@labard.fr>
2022-05-25 12:21:50 +0200

    feat: ✨ add the method providePaprikaApi

--- app/src/main/java/fr/labard/cryptocurrencyapp/di/AppModule.kt
+++ app/src/main/java/fr/labard/cryptocurrencyapp/di/AppModule.kt
@@ -6,6 +6,8 @@ import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import fr.labard.cryptocurrencyapp.common.Constants
 import fr.labard.cryptocurrencyapp.data.remote.CoinPaprikaApi
+import fr.labard.cryptocurrencyapp.data.repository.CoinRepositoryImpl
+import fr.labard.cryptocurrencyapp.domain.repository.CoinRepository
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
@@ -20,4 +22,8 @@ object AppModule {
         .addConverterFactory(GsonConverterFactory.create())
         .build()
         .create(CoinPaprikaApi::class.java)
+
+    @Provides
+    @Singleton
+    fun provideCoinRespository(api: CoinPaprikaApi): CoinRepository = CoinRepositoryImpl(api)
 }
