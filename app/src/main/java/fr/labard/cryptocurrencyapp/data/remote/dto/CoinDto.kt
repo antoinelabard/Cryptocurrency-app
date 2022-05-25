c05d19b3858ef03efdc7bbe9c3543d026f5d522f
Antoine Labard <antoine@labard.fr>
2022-05-25 09:52:45 +0200

    feat: ✨ add CoinDto and Coin classes

---
+++ app/src/main/java/fr/labard/cryptocurrencyapp/data/remote/dto/CoinDto.kt
@@ -0,0 +1,24 @@
+package fr.labard.cryptocurrencyapp.data.remote.dto
+
+import com.google.gson.annotations.SerializedName
+import fr.labard.cryptocurrencyapp.domain.model.Coin
+
+data class CoinDto(
+    val id: String,
+    @SerializedName("is_active")
+    val isActive: Boolean,
+    @SerializedName("is_new")
+    val isNew: Boolean,
+    val name: String,
+    val rank: Int,
+    val symbol: String,
+    val type: String
+)
+
+fun CoinDto.toCoin() = Coin(
+    id = id,
+    isActive = isActive,
+    name = name,
+    rank = rank,
+    symbol = symbol
+)
