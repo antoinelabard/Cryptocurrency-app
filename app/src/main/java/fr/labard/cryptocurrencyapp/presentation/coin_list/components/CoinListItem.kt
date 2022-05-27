93e5c5a5a6d5baa85d988fbc3c703f671629e76b
Antoine Labard <antoine@labard.fr>
2022-05-27 18:47:11 +0200

    fix: 🐛 add a missing argument to onItemClick

--- app/src/main/java/fr/labard/cryptocurrencyapp/presentation/coin_list/components/CoinListItem.kt
+++ app/src/main/java/fr/labard/cryptocurrencyapp/presentation/coin_list/components/CoinListItem.kt
@@ -24,7 +24,7 @@ fun CoinListItem(
     Row(
         modifier = Modifier
             .fillMaxWidth()
-            .clickable { onItemClick }
+            .clickable { onItemClick(coin) }
             .padding(20.dp),
         horizontalArrangement = Arrangement.SpaceBetween
     ) {
