b9cf062d43a88170c38c65895a26ba0fb118dcc4
Antoine Labard <antoine@labard.fr>
2022-05-27 18:46:20 +0200

    fix: 🐛 typo in the getCoinById url

--- app/src/main/java/fr/labard/cryptocurrencyapp/data/remote/CoinPaprikaApi.kt
+++ app/src/main/java/fr/labard/cryptocurrencyapp/data/remote/CoinPaprikaApi.kt
@@ -9,6 +9,6 @@ interface CoinPaprikaApi {
     @GET("/v1/coins")
     suspend fun getCoins(): List<CoinDto>
 
-    @GET("/v1/coins/{coindId}")
+    @GET("/v1/coins/{coinId}")
     suspend fun getCoinById(@Path("coinId") coindId: String): CoinDetailDto
 }
